6c0544f7fc321072e25c7d61f6fdc79bf4c1f57e
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-04-17 23:15:42 -0300

    menu nav responsivo

---
+++ front-upper/src/app/nav/nav.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  private navItems = [
+    { "id": "home", "title": "Home", "status": true },
+    { "id": "servicos", "title": "Serviços", "status": false },
+    { "id": "nos", "title": "Nós", "status": false },
+    { "id": "sobre", "title": "Contato", "status": false },
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+  getStatus(item) {
+    return item.value.status;
+  }
+  changeStatus(item) {
+    for (let key in this.navItems) {
+      if (item.value.id == this.navItems[key].id) {
+        this.navItems[key].status = true;
+      } else {
+        this.navItems[key].status = false
+      }
+    }
+  }
+
+  getRef(item) {
+    return item.value.id
+  }
+
+}
