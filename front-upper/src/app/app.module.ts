e7d740f7c783400edf194f1fb9e3022444948d7d
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-04-19 00:50:10 -0300

    Tela de login

--- front-upper/src/app/app.module.ts
+++ front-upper/src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './page/nav/nav.component';
 import { PageComponent } from './page/page.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: '', component: PageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    PageComponent
+    PageComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
